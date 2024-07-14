3fde7c8f2058b562fd1e04e7ea8538f2def1519f
PaulOberg1 <pauloberg1931@gmail.com>
2024-07-14 11:37:17 +0100

    Refactor: Implement header files for layers
    - Defined structure of ConvLayer and DenseLayer classes in ConveLayer.h and DenseLayer.h respectively

---
+++ include/ConvLayer.h
@@ -0,0 +1,23 @@
+#ifndef CONVLAYER_H
+#define CONVLAYER_H
+
+#include <Eigen/Dense>
+
+class ConvLayer{
+private:
+    Eigen::MatrixXf kernels;
+    Eigen::MatrixXf biases;
+
+public:
+    ConvLayer(int prevMatLength, int kernelLength) {}
+
+    void initWeights(int prevMatLength, int kernelLength) {}
+
+    void forwardProp() {}
+
+    void backProp() {}
+    
+    void gradDesc() {}
+};
+
+#endif
---
+++ include/DenseLayer.h
@@ -0,0 +1,24 @@
+#ifndef DENSELAYER_H
+#define DENSELAYER_H
+
+#include <Eigen/Dense>
+
+class DenseLayer{
+private:
+    Eigen::MatrixXf weights;
+    Eigen::MatrixXf biases;
+
+public:
+    DenseLayer(int prevLayerNodes, int curLayerNodes) {}
+
+    void initWeights(int prevLayerNodes, int curLayerNodes) {}
+
+    void forwardProp() {}
+
+    void backProp() {}
+
+    void gradDesc() {}
+    
+};
+
+#endif
