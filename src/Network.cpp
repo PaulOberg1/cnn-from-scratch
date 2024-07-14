03f2e815f82bbecfaabd07df65aa40bcdfde45b1
PaulOberg1 <pauloberg1931@gmail.com>
2024-07-14 12:39:10 +0100

    Feature: Implement forward propagation in Network
    - Modified Network constructor to match declaration in Network.h
    - Implemented definition for "forwardProp" method

--- src/Network.cpp
+++ src/Network.cpp
@@ -1,22 +1,23 @@
 #include "Network.h"
 
-Network::Network(std::map<std::string,std::map<std::string,std::pair<int,int>>> dimensions) {
-    std::pair<float,float> ConvKernelDims = dimensions.at("ConvLayer").at("Kernels");
-    std::pair<float,float> ConvBiasDims = dimensions.at("ConvLayer").at("Biases");
+Network::Network(int Conv1Nodes, int Conv2Nodes, int Dense1Nodes, int Dense2Nodes) {
+    m_convLayer1 = new ConvLayer(1,Conv1Nodes);
+    m_convLayer2 = new ConvLayer(Conv1Nodes,Conv2Nodes);
 
-    std::pair<float,float> DenseWeightDims = dimensions.at("DenseLayer").at("Kernels");
-    std::pair<float,float> DenseBiasDims = dimensions.at("DenseLayer").at("Biases");
-
-    convKernels = Eigen::MatrixXf::Random(ConvKernelDims.first,ConvKernelDims.second);
-    convBiases = Eigen::MatrixXf::Random(ConvBiasDims.first,ConvBiasDims.second);
-
-    denseWeights = Eigen::MatrixXf::Random(DenseWeightDims.first,DenseWeightDims.second);
-    denseBiases = Eigen::MatrixXf::Random(DenseBiasDims.first,DenseBiasDims.second);
+    m_denseLayer1 = new DenseLayer(Conv2Nodes,Dense1Nodes);
+    m_denseLayer2 = new DenseLayer(Dense1Nodes, Dense2Nodes);
 }
 
-void Network::run() {}
+void Network::run(Eigen::MatrixXf X) {}
 
-void Network::forwardProp() {}
+Eigen::MatrixXf Network::forwardProp(Eigen::MatrixXf X) {
+    m_convLayer1->forwardProp(X);
+    m_convLayer2->forwardProp(m_convLayer1->getA());
+    m_denseLayer1->forwardProp(m_convLayer2->getA());
+    m_denseLayer2->forwardProp(m_denseLayer1->getA());
+
+    return m_denseLayer2->getA();
+}
 
 void Network::backProp() {}
 
