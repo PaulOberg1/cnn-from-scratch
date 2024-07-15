379352104785e8891cd92411a1745a7691d02cb1
PaulOberg1 <pauloberg1931@gmail.com>
2024-07-15 11:37:23 +0100

    Feature: Add activation function definitions
    - Created activations.h to declare sigmoid and ReLU activation functions
    - Created activations.cpp to define activation functions

---
+++ include/activations.h
@@ -0,0 +1,5 @@
+#include <Eigen/Dense>
+
+const Eigen::MatrixXf& sigmoid(const Eigen::MatrixXf& mat);
+
+const Eigen::MatrixXf& ReLU(const Eigen::MatrixXf& mat);
---
+++ src/activations.cpp
@@ -0,0 +1,23 @@
+#include "activations.h"
+
+const Eigen::MatrixXf& sigmoid(const Eigen::MatrixXf& mat) {
+    Eigen::MatrixXf returnMat;
+    returnMat.resize(mat.rows(), mat.cols());
+    for (int i=0; i<returnMat.rows(); i++) {
+        for (int j=0; j<returnMat.cols(); j++) {
+            returnMat(i,j) = 1 / (1 + std::exp(-mat(i,j)));
+        }
+    }
+    return returnMat;
+}
+
+const Eigen::MatrixXf& ReLU(const Eigen::MatrixXf& mat) {
+    Eigen::MatrixXf returnMat;
+    returnMat.resize(mat.rows(), mat.cols());
+    for (int i=0; i<returnMat.rows(); i++) {
+        for (int j=0; j<returnMat.cols(); j++) {
+            returnMat(i, j) = mat(i,j) > 0 ? mat(i,j) : 0;
+        }
+    }
+    return returnMat;
+}
