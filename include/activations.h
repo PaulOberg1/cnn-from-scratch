72107144c4cd70566845d06be1977d5038623981
PaulOberg1 <pauloberg1931@gmail.com>
2024-07-16 13:15:04 +0100

    Feature: Add derivatives of activation functions
    - Defined derivatives of ReLU and Sigmoid functions
    - Implemented function to map given functions to their derivatives

--- include/activations.h
+++ include/activations.h
@@ -1,5 +1,20 @@
 #include <Eigen/Dense>
+#include <unordered_map>
+#include <string>
+
+using MatTransformFunc = std::function<Eigen::MatrixXf(const Eigen::MatrixXf&)>;
 
 const Eigen::MatrixXf& sigmoid(const Eigen::MatrixXf& mat);
 
 const Eigen::MatrixXf& ReLU(const Eigen::MatrixXf& mat);
+
+const Eigen::MatrixXf& deriveSigmoid(const Eigen::MatrixXf& mat);
+
+const Eigen::MatrixXf& deriveReLU(const Eigen::MatrixXf& mat);
+
+const MatTransformFunc& getInverse(const MatTransformFunc& func);
+
+std::unordered_map<MatTransformFunc, MatTransformFunc> functionToInverseMap{
+    {sigmoid,deriveSigmoid},
+    {ReLU,deriveReLU}
+};
--- src/activations.cpp
+++ src/activations.cpp
@@ -21,3 +21,24 @@ const Eigen::MatrixXf& ReLU(const Eigen::MatrixXf& mat) {
     }
     return returnMat;
 }
+
+const Eigen::MatrixXf& deriveSigmoid(const Eigen::MatrixXf& mat) {
+    Eigen::MatrixXf sig = sigmoid(mat);
+    return sig.array() * (1.0f - sig.array());
+}
+
+const Eigen::MatrixXf& deriveReLU(const Eigen::MatrixXf& mat) {
+    int cols = mat.cols();
+    int rows = mat.rows();
+    Eigen::MatrixXf outMat(cols,rows);
+
+    for (int i=0; i<cols; i++) {
+        for (int j=0; j<rows; j++)
+            outMat[i,j] = mat[i,j] > 0;
+    }
+    return outMat;
+}
+
+const MatTransformFunc& getInverse(const MatTransformFunc& func) {
+    return functionToInverseMap[func];
+}
