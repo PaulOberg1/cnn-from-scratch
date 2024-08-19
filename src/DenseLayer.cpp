e04191ec131d2846fd5bfe7dabd3d44e73491cc4
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-19 10:46:41 +0100

    Feature: Add He Initialisation in Dense Layers
    - Implemented He initialisation of kernels and zero-initialisation of biases in initWeights method
    - Ensured correct transposition of matrices in forward and back propagation to correct matrix shapes
    - Added assert statements and try-catch blocks for improved error-handling/debugging
    - Ensured element-wise multiplication in calculation of m_dA in backProp method
    - Modified calls to activation derivative functions to ensure valid argument list

--- src/DenseLayer.cpp
+++ src/DenseLayer.cpp
@@ -1,36 +1,53 @@
 #include "DenseLayer.h"
+#include <iostream>
 
 DenseLayer::DenseLayer(int prevLayerNodes, int curLayerNodes, const ActivationFunc& activation) : m_activation(activation) {
     initWeights(prevLayerNodes,curLayerNodes);
 }
 
 void DenseLayer::initWeights(int prevLayerNodes, int curLayerNodes) {
-    m_weights = Eigen::MatrixXf::Random(curLayerNodes,prevLayerNodes);
-    m_biases = Eigen::MatrixXf::Random(curLayerNodes,1);
+    assert(curLayerNodes>0 && prevLayerNodes>0);
+    m_weights = ((Eigen::MatrixXf::Random(curLayerNodes,prevLayerNodes).array()+1.0f)/2.0f) * std::sqrt(2.0f / curLayerNodes);
+    m_biases = Eigen::MatrixXf(curLayerNodes,1);
+    m_biases.setZero();
 }
 
 void DenseLayer::forwardProp(Eigen::MatrixXf X) {
-    m_Z = m_weights * X + m_biases;
-    m_A = m_activation(m_Z);
+    assert(m_weights.cols()==X.rows());
+    try {
+        m_Z = m_weights * X + m_biases;
+        m_A = m_activation(m_Z);
+        
+    } catch (const std::exception& e) {
+        std::cerr<<"Caught exception in DenseLayer::forwardProp: "<<e.what();
+    }
 }
 
 void DenseLayer::backProp(Eigen::MatrixXf nextLayerW, Eigen::MatrixXf nextLayerDz, Eigen::MatrixXf prevLayerA) {
-
-    ActivationFunc activationDerivative = getActFuncDeriv(m_activation);
-
-    m_dA = nextLayerW * nextLayerDz;
-    m_dZ = activationDerivative(m_A) * m_dA;
-    m_dW = m_dZ * prevLayerA;
-    m_dB = m_dZ;
+    try{
+        m_dA = nextLayerDz(0,0) * nextLayerW.array();
+        m_dZ = getActFuncDeriv(m_activation)(m_A, m_dA.transpose());
+        m_dW = m_dZ * prevLayerA.transpose();
+        
+        m_dB = m_dZ;
+    } catch (const std::exception& e) {
+        std::cerr<<"Caught exception in DenseLayer::backProp: "<<e.what();
+    }
 }
 
 void DenseLayer::backProp(Eigen::MatrixXf Y, Eigen::MatrixXf prevLayerA) {
-    m_dZ = m_A - Y;
-    m_dW = m_dZ * prevLayerA;
-    m_dB = m_dZ;
+    try { 
+
+        m_dZ = m_A - Y;
+        m_dW = m_dZ(0,0) * prevLayerA.transpose();
+        m_dB = m_dZ;
+    } catch (const std::exception& e) {
+        std::cerr<<"Caught exception in DenseLayer::backProp: "<<e.what();
+    }
 }
 
-void DenseLayer::gradDesc(int learningRate) {
+void DenseLayer::gradDesc(double learningRate) {
+    
     m_weights -= learningRate*m_dW;
     m_biases -= learningRate*m_dB;
 }
