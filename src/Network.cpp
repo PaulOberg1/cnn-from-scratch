a19a6ba84f4aa986492e08056d56bda5621a5731
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-19 11:14:00 +0100

    Bugfix: Fix forward and backpropagation in Network
    - Ensure correct arguments in calls to forward and backpropagation in convolutional and dense layers
    - Condensed output size calculations in Network constructor to leverage new getOutputSize and getFlattenedP method definitions
    - Prevent loss of precision of learning rate by removing integer casts in parameter lists

--- src/Network.cpp
+++ src/Network.cpp
@@ -8,19 +8,16 @@ Network::Network(const LayerData& layerData, Eigen::MatrixXf X, Eigen::MatrixXf 
     DenseLayerData d1Data = layerData.d1Data;
     DenseLayerData d2Data = layerData.d2Data;
     
-    m_convLayer1 = std::make_unique<ConvLayer>(1, c1Data.kernelSize, c1Data.activation, c1Data.pool, 2, 2);
-    m_convLayer2 = std::make_unique<ConvLayer>(1, c2Data.kernelSize, c2Data.activation, c2Data.pool,2, 2);
+    m_convLayer1 = std::make_unique<ConvLayer>(X.rows(), c1Data.kernelSize, c1Data.activation, c1Data.pool, 2, 2);    
+    m_convLayer2 = std::make_unique<ConvLayer>(m_convLayer1->getOutputSize(), c2Data.kernelSize, c2Data.activation, c2Data.pool,2, 2);
 
-    std::pair<int,int> XDims(X.cols(),X.rows());
-    std::pair<int,int> C1OutDims = m_convLayer1->getOutputSize(XDims);
-    std::pair<int,int> C2OutDims = m_convLayer2->getOutputSize(C1OutDims);
-    int flattenedOutputSize = C2OutDims.first * C2OutDims.second;
+    int flattenedOutputSize = pow(m_convLayer2->getOutputSize(),2);
 
     m_denseLayer1 = std::make_unique<DenseLayer>(flattenedOutputSize, d1Data.numNodes, d1Data.activation);
     m_denseLayer2 = std::make_unique<DenseLayer>(d1Data.numNodes, d2Data.numNodes, d2Data.activation);
 }
 
-Eigen::MatrixXf Network::run(int numIterations, int learningRate) {
+Eigen::MatrixXf Network::run(int numIterations, double learningRate) {
     for (int i=0; i<numIterations; i++) {
         Eigen::MatrixXf y_pred = forwardProp();
         backProp();
@@ -31,8 +28,8 @@ Eigen::MatrixXf Network::run(int numIterations, int learningRate) {
 
 Eigen::MatrixXf Network::forwardProp() {
     m_convLayer1->forwardProp(m_X);
-    m_convLayer2->forwardProp(m_convLayer1->getA());
-    m_denseLayer1->forwardProp(m_convLayer2->getA());
+    m_convLayer2->forwardProp(m_convLayer1->getP());
+    m_denseLayer1->forwardProp(m_convLayer2->getFlattenedP());
     m_denseLayer2->forwardProp(m_denseLayer1->getA());
 
     return m_denseLayer2->getA();
@@ -40,17 +37,17 @@ Eigen::MatrixXf Network::forwardProp() {
 
 void Network::backProp() {
 
-    m_denseLayer2->backProp(m_Y, m_denseLayer2->getA());
+    m_denseLayer2->backProp(m_Y, m_denseLayer1->getA());
 
-    m_denseLayer1->backProp(m_denseLayer2->getW(), m_denseLayer2->getDz(), m_convLayer2->getA());
+    m_denseLayer1->backProp(m_denseLayer2->getW(), m_denseLayer2->getDz(), m_convLayer2->getFlattenedP());
 
-    m_convLayer2->backProp(m_denseLayer1->getW(),m_denseLayer1->getDz(),m_convLayer1->getA());
+    m_convLayer2->backProp(m_denseLayer1->getW(),m_denseLayer1->getDz(),m_convLayer1->getP(),true);
 
-    m_convLayer1->backProp(m_convLayer2->getK(), m_convLayer2->getDz(), m_X);
+    m_convLayer1->backProp(m_convLayer2->getK(), m_convLayer2->getDz(), m_X, false);
 
 }
 
-void Network::gradDesc(int learningRate) {
+void Network::gradDesc(double learningRate) {
     m_convLayer1->gradDesc(learningRate);
     m_convLayer2->gradDesc(learningRate);
     m_denseLayer1->gradDesc(learningRate);
