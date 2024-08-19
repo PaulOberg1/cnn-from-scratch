57a7726f16a093661a533a310f5b305216cfb6c4
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-19 10:38:00 +0100

    Feature: Create new type aliases in GetFuncDeriv.h
    - Defined 'ActivationFuncDeriv' and 'PoolFuncDeriv' type aliases
    - Changed return types for 'getActFunc' and 'getPoolFunc' declarations to new type aliases

--- include/helper_functions/GetFuncDeriv.h
+++ include/helper_functions/GetFuncDeriv.h
@@ -5,9 +5,11 @@
 #include <unordered_map>
 
 using ActivationFunc = std::function<Eigen::MatrixXf(const Eigen::MatrixXf&)>;
+using ActivationFuncDeriv = std::function<Eigen::MatrixXf(const Eigen::MatrixXf&, const Eigen::MatrixXf&)>;
 using PoolFunc = std::function<Eigen::MatrixXf(const Eigen::MatrixXf&, int, int)>;
+using PoolFuncDeriv = std::function<Eigen::MatrixXf(const Eigen::MatrixXf&, const Eigen::MatrixXf&, int, int)>;
 
-ActivationFunc getActFuncDeriv(const ActivationFunc& func);
-PoolFunc getPoolFuncDeriv(const PoolFunc& func);
+ActivationFuncDeriv getActFuncDeriv(const ActivationFunc& func);
+PoolFuncDeriv getPoolFuncDeriv(const PoolFunc& func);
 
 #endif
