c5d9a7c49ac02856b929d9d560e021a112061533
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-27 19:19:28 +0100

    Bugfix: Ensure valid function calls in main
    - Updated fields in layer data structs to fix type ambiguity
    - Converted X to a 3D matrix and initialsed it accordingly, using He Initialisation

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,30 @@
 #include <iostream>
 #include <Eigen/Dense>
 
+#include "trainMain.h"
+#include "testMain.h"
 #include "Network.h"
 #include "LayerData.h"
 #include "helper_functions/ActivationFuncs.h"
 #include "helper_functions/PoolFuncs.h"
+#include <omp.h>
 
-int main() {
+
+int main() {    
+
     ConvLayerData c1Data;
-    c1Data.activation = ReLU;
-    c1Data.activationDeriv = deriveReLU;
+    c1Data.activation = ReLU3D;
+    c1Data.activationDeriv = deriveReLU3D;
     c1Data.pool = maxPool;
     c1Data.poolDeriv = deriveMaxPool;
-    c1Data.kernelSize=3;
+    c1Data.kernelDimensions=std::vector<int>({32,3,3,3});
 
     ConvLayerData c2Data;
-    c2Data.activation = ReLU;
-    c2Data.activationDeriv = deriveReLU;
+    c2Data.activation = ReLU3D;
+    c2Data.activationDeriv = deriveReLU3D;
     c2Data.pool = maxPool;
     c2Data.poolDeriv = deriveMaxPool;
-    c2Data.kernelSize=3;
+    c2Data.kernelDimensions=std::vector<int>({64,32,3,3});
 
     DenseLayerData d1Data;
     d1Data.activation = ReLU;
@@ -37,13 +42,18 @@ int main() {
     layerData.d1Data = d1Data;
     layerData.d2Data = d2Data;
 
-    Eigen::MatrixXf X = (Eigen::MatrixXf::Random(66,66).array()+1.0f)/2.0f;
+    std::vector<Eigen::MatrixXf> X = {};
+    for (int i=0; i<3; i++) {
+        Eigen::MatrixXf subMat = Eigen::MatrixXf::Random(66,66).array()+1.0f/2.0f;
+        X.push_back(subMat);
+    }
     Eigen::MatrixXf Y(1,1);
     Y << 0.8109;
     try{
-        Network CNN(layerData,X,Y);
-
-        Eigen::MatrixXf y_pred = CNN.run(100,0.01);
+        std::vector<int> inputMatDimensions({3,66,66});
+        Network CNN = Network(layerData,inputMatDimensions);
+        trainMain("C:/EggSpector/data",CNN,inputMatDimensions);
+        //CNN.storeData();
     } catch (const std::exception& e) {
         std::cerr<<"Caught exception in Network constructor or run method: "<<e.what();
     }
