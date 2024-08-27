ab4d93b0b41150d4a5a7672a991d0669a9e02dfa
PaulOberg1 <pauloberg1931@gmail.com>
2024-08-27 19:14:42 +0100

    Refactor: Remove 2D overloads of pool functions
    - Removed redundant overloads of maxPool and avgPool functions, and their derivatives, that operate on 3D matrices

--- include/helper_functions/PoolFuncs.h
+++ include/helper_functions/PoolFuncs.h
@@ -4,17 +4,13 @@
 #include <Eigen/Dense>
 #include <unsupported/Eigen/CXX11/Tensor>
 
-Eigen::MatrixXf avgPool(const Eigen::MatrixXf& mat, int stride=2, int poolSize=2);
 std::vector<Eigen::MatrixXf> avgPool(const std::vector<Eigen::MatrixXf>& mat, int stride=2, int poolSize=2);
 
 
-Eigen::MatrixXf maxPool(const Eigen::MatrixXf& mat, int stride=2, int poolSize=2);
 std::vector<Eigen::MatrixXf> maxPool(const std::vector<Eigen::MatrixXf>& mat, int stride=2, int poolSize=2);
 
-Eigen::MatrixXf deriveAvgPool(const Eigen::MatrixXf& mat, const Eigen::MatrixXf& grad, int stride=2, int poolSize=2);
 std::vector<Eigen::MatrixXf> deriveAvgPool(const std::vector<Eigen::MatrixXf>& mat, const std::vector<Eigen::MatrixXf>& grad, int stride=2, int poolSize=2);
 
-Eigen::MatrixXf deriveMaxPool(const Eigen::MatrixXf& mat, const Eigen::MatrixXf& grad, int stride=2, int poolSize=2);
 std::vector<Eigen::MatrixXf> deriveMaxPool(const std::vector<Eigen::MatrixXf>& mat, const std::vector<Eigen::MatrixXf>& grad, int stride=2, int poolSize=2);
 
 #endif
--- src/helper_functions/PoolFuncs.cpp
+++ src/helper_functions/PoolFuncs.cpp
@@ -1,22 +1,6 @@
 #include "helper_functions/PoolFuncs.h"
 
-Eigen::MatrixXf avgPool(const Eigen::MatrixXf& mat, int stride, int poolSize) {
-    int rows = mat.rows();
-    int cols = mat.cols();
-
-    int newRows = rows/stride;
-    int newCols = cols/stride;
-
-    Eigen::MatrixXf returnMat (newRows,newCols);
-
-    for (int i=0; i<newRows; i++) {
-        for (int j=0; j<newCols; j++) {
-            returnMat(i, j) = mat.block(i*stride,j*stride,poolSize,poolSize).mean();
-        }
-    }
-    return returnMat;
-}
-std::vector<Eigen::MatrixXf> avgPool(const std::vector<Eigen::MatrixXf>& mat, int stride=2, int poolSize=2) {
+std::vector<Eigen::MatrixXf> avgPool(const std::vector<Eigen::MatrixXf>& mat, int stride, int poolSize) {
     int depth = mat.size();
     int height = mat.at(0).rows();
     int width = mat.at(0).cols();
@@ -40,24 +24,7 @@ std::vector<Eigen::MatrixXf> avgPool(const std::vector<Eigen::MatrixXf>& mat, in
     return returnMat;
 }
 
-
-Eigen::MatrixXf maxPool(const Eigen::MatrixXf& mat, int stride, int poolSize) {
-    int rows = mat.rows();
-    int cols = mat.cols();
-
-    int newRows = rows/2;
-    int newCols = cols/2;
-
-    Eigen::MatrixXf returnMat (newRows,newCols);
-
-    for (int i=0; i<newRows; i++) {
-        for (int j=0; j<newCols; j++) {
-            returnMat(i, j) = mat.block(i*stride,j*stride,poolSize,poolSize).maxCoeff();
-        }
-    }
-    return returnMat;
-}
-std::vector<Eigen::MatrixXf> maxPool(const std::vector<Eigen::MatrixXf>& mat, int stride=2, int poolSize=2) {
+std::vector<Eigen::MatrixXf> maxPool(const std::vector<Eigen::MatrixXf>& mat, int stride, int poolSize) {
     int depth = mat.size();
     int height = mat.at(0).rows();
     int width = mat.at(0).cols();
@@ -81,24 +48,6 @@ std::vector<Eigen::MatrixXf> maxPool(const std::vector<Eigen::MatrixXf>& mat, in
     return returnMat;
 }
 
-Eigen::MatrixXf deriveAvgPool(const Eigen::MatrixXf& mat, const Eigen::MatrixXf& grad, int stride, int poolSize) {
-    int rows = mat.rows();
-    int cols = mat.cols();
-
-    int newRows = rows*stride;
-    int newCols = cols*stride;
-
-    Eigen::MatrixXf returnMat(newRows,newCols);
-    returnMat.setZero();
-
-    for (int i=0; i<rows; i++) {
-        for (int j=0; j<cols; j++) {
-            float gradValue = mat(i,j) / float(poolSize*poolSize);
-            returnMat.block(i*stride, j*stride, poolSize, poolSize).array()+=gradValue;
-        }
-    }
-    return returnMat;
-}
 std::vector<Eigen::MatrixXf> deriveAvgPool(const std::vector<Eigen::MatrixXf>& mat, const std::vector<Eigen::MatrixXf>& grad, int stride, int poolSize) {
     int depth = mat.size();
     int height = mat.at(0).rows();
@@ -132,20 +81,6 @@ std::vector<Eigen::MatrixXf> deriveAvgPool(const std::vector<Eigen::MatrixXf>& m
     return returnMat;
 }
 
-Eigen::MatrixXf deriveMaxPool(const Eigen::MatrixXf& mat, const Eigen::MatrixXf& grad, int stride, int poolSize) {
-    int rows = mat.rows();
-    int cols = mat.cols();
-
-    Eigen::MatrixXf returnMat (rows, cols);
-
-    for (int i=0; i<rows; i++) {
-        for (int j=0; j<cols; j++) {
-            float maxVal = mat.block(i/stride, j/stride, poolSize, poolSize).maxCoeff();
-            returnMat(i,j) = (mat(i,j) == maxVal) ? 1.0f : 0.0f;
-        }
-    }
-    return returnMat;
-}
 std::vector<Eigen::MatrixXf> deriveMaxPool(const std::vector<Eigen::MatrixXf>& mat, const std::vector<Eigen::MatrixXf>& grad, int stride, int poolSize) {
     int depth = mat.size();
     int height = mat.at(0).rows();
