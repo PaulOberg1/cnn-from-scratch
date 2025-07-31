f36be89f116f5c06fcbc10a8ec9e2078f1290860
PaulOberg1 <pauloberg1931@gmail.com>
2025-07-31 19:24:23 +0100

    Feature: Define runNetwork in main.cpp - Runs the CNN with a given image path string

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+
+
 #include <iostream>
 #include <Eigen/Dense>
 
@@ -10,37 +12,96 @@
 #include <omp.h>
 
 
-int main() {    
 
+int runNetwork(std::string path) {
     ConvLayerData c1Data;
-    c1Data.activation = ReLU3D;
-    c1Data.activationDeriv = deriveReLU3D;
+    c1Data.activation = LeakyReLU3D;
+    c1Data.activationDeriv = deriveLeakyReLU3D;
     c1Data.pool = maxPool;
     c1Data.poolDeriv = deriveMaxPool;
     c1Data.kernelDimensions=std::vector<int>({32,3,3,3});
 
     ConvLayerData c2Data;
-    c2Data.activation = ReLU3D;
-    c2Data.activationDeriv = deriveReLU3D;
+    c2Data.activation = LeakyReLU3D;
+    c2Data.activationDeriv = deriveLeakyReLU3D;
     c2Data.pool = maxPool;
     c2Data.poolDeriv = deriveMaxPool;
     c2Data.kernelDimensions=std::vector<int>({64,32,3,3});
 
     DenseLayerData d1Data;
-    d1Data.activation = ReLU;
-    d1Data.activationDeriv = deriveReLU;
-    d1Data.numNodes = 16;
+    d1Data.activation = LeakyReLU;
+    d1Data.activationDeriv = deriveLeakyReLU;
+    d1Data.numNodes = 64;
 
     DenseLayerData d2Data;
-    d2Data.activation = sigmoid;
-    d2Data.activationDeriv = deriveSigmoid;
-    d2Data.numNodes = 1;
+    d2Data.activation = LeakyReLU;
+    d2Data.activationDeriv = deriveLeakyReLU;
+    d2Data.numNodes = 16;
+
+    DenseLayerData d3Data;
+    d3Data.activation = sigmoid;
+    d3Data.activationDeriv = deriveSigmoid;
+    d3Data.numNodes = 1;
 
     LayerData layerData;
     layerData.c1Data = c1Data;
     layerData.c2Data = c2Data;
     layerData.d1Data = d1Data;
     layerData.d2Data = d2Data;
+    layerData.d3Data = d3Data;
+
+    std::vector<int> inputMatDimensions({3,66,66});
+    Network CNN = Network(layerData,inputMatDimensions);
+
+    int res = testImg(path,CNN,inputMatDimensions);
+    return res==1 ? true : false;
+}
+
+
+extern "C" {
+    __declspec(dllexport) int simpleFunction(const char* path) {
+        return runNetwork(path); // Just return a constant integer
+    }
+}
+
+
+int main() {    
+
+    ConvLayerData c1Data;
+    c1Data.activation = LeakyReLU3D;
+    c1Data.activationDeriv = deriveLeakyReLU3D;
+    c1Data.pool = maxPool;
+    c1Data.poolDeriv = deriveMaxPool;
+    c1Data.kernelDimensions=std::vector<int>({32,3,3,3});
+
+    ConvLayerData c2Data;
+    c2Data.activation = LeakyReLU3D;
+    c2Data.activationDeriv = deriveLeakyReLU3D;
+    c2Data.pool = maxPool;
+    c2Data.poolDeriv = deriveMaxPool;
+    c2Data.kernelDimensions=std::vector<int>({64,32,3,3});
+
+    DenseLayerData d1Data;
+    d1Data.activation = LeakyReLU;
+    d1Data.activationDeriv = deriveLeakyReLU;
+    d1Data.numNodes = 64;
+
+    DenseLayerData d2Data;
+    d2Data.activation = LeakyReLU;
+    d2Data.activationDeriv = deriveLeakyReLU;
+    d2Data.numNodes = 16;
+
+    DenseLayerData d3Data;
+    d3Data.activation = sigmoid;
+    d3Data.activationDeriv = deriveSigmoid;
+    d3Data.numNodes = 1;
+
+    LayerData layerData;
+    layerData.c1Data = c1Data;
+    layerData.c2Data = c2Data;
+    layerData.d1Data = d1Data;
+    layerData.d2Data = d2Data;
+    layerData.d3Data = d3Data;
 
     std::vector<Eigen::MatrixXf> X = {};
     for (int i=0; i<3; i++) {
